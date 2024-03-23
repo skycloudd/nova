e7da11f54e4022e4aaa1340e20ce6fafcc08d309
skycloudd <51929172+skycloudd@users.noreply.github.com>
2024-03-23 02:06:21 +0100

    better examples

---
+++ examples/diagnostics/run_all.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cargo build
+
+# for every file ending in .nv in the current directory
+# run `bat <file>`
+# run `cargo run -- <file>`
+
+for file in *.nv; do
+    echo "\n"
+    bat $file -l rb
+    ../../target/debug/nova $file
+done
